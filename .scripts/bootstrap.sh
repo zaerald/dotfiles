1b90e1a5915fbb29ce8c67ce84044cbb511a6bc2
zaerald <zaerald.zd@gmail.com>
2022-07-25 01:53:13 +0800

    feat(.scripts): add bootstrap installation

---
+++ .scripts/bootstrap.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+brew install node
+brew install ranger
+brew install tmux
+
+brew install diff-so-fancy
+brew install fd
+brew install ripgrep
+
+# version managers
+brew install jenv
+brew install nvm
+
+# neovim
+brew install ninja libtool automake cmake pkg-config gettext curl
+brew install perl
+brew install tree-sitter
+
+# misc
+brew install neofetch
+
+## applications ##
+brew install --cask alfred
+brew install --cask anki
+brew install --cask arduino
+brew install --cask calibre
+brew install --cask discord
+brew install --cask intellij-idea-ce
+brew install --cask iterm2
+brew install --cask itsycal
+brew install --cask notion
+brew install --cask remnote
+brew install --cask spotify
+brew install --cask todoist
+brew install --cask vlc
+

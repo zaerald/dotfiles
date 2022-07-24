b3fa1ce2535c8b9679d273edafc4bb1899611e54
zaerald <zaerald.zd@gmail.com>
2022-07-25 02:01:21 +0800

    feat(.scripts): migrate .Brewfile to bootstrap.sh

--- .scripts/bootstrap.sh
+++ .scripts/bootstrap.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# langs
+brew install go
 brew install node
+brew install perl
+
+# version managers
+brew install jenv
+brew install nvm
+
+# tools
+brew install docker
+brew install htop
 brew install ranger
 brew install tmux
 
@@ -8,13 +19,8 @@ brew install diff-so-fancy
 brew install fd
 brew install ripgrep
 
-# version managers
-brew install jenv
-brew install nvm
-
 # neovim
 brew install ninja libtool automake cmake pkg-config gettext curl
-brew install perl
 brew install tree-sitter
 
 # misc
@@ -26,10 +32,15 @@ brew install --cask anki
 brew install --cask arduino
 brew install --cask calibre
 brew install --cask discord
+brew install --cask firefox
+brew install --cask google-chrome
 brew install --cask intellij-idea-ce
 brew install --cask iterm2
 brew install --cask itsycal
+brew install --cask keycastr
 brew install --cask notion
+brew install --cask postman
+brew install --cask rectangle
 brew install --cask remnote
 brew install --cask spotify
 brew install --cask todoist

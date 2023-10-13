e062872dbdd0541a1e4667fef03439ae3614598a
zaerald <zaerald.zd@gmail.com>
2023-10-13 16:55:47 +0800

    feat(nvim): configure lazyvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,33 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+vim.keymap.set("n", "<C-h>", "<Cmd>TmuxNavigateLeft<CR>", { silent = true })
+vim.keymap.set("n", "<C-j>", "<Cmd>TmuxNavigateDown<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", "<Cmd>TmuxNavigateUp<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", "<Cmd>TmuxNavigateRight<CR>", { silent = true })
+vim.keymap.set("n", "<C-\\>", "<Cmd>TmuxNavigateLastActive<CR>", { silent = true })
+vim.keymap.set("n", "<C-Space>", "<Cmd>TmuxNavigateNavigateNext<CR>", { silent = true })
+
+-- toggles
+vim.keymap.set("n", "<leader>uH", "<cmd>set hlsearch!<CR><Bar><Cmd>set hlsearch?<CR>", { silent = true })
+
+-- navigation
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
+vim.keymap.set("n", "n", "nzzzv", { silent = true })
+vim.keymap.set("n", "N", "Nzzzv", { silent = true })
+vim.keymap.set("n", "H", "zH", { silent = true })
+vim.keymap.set("v", "H", "zH", { silent = true })
+vim.keymap.set("n", "L", "zL", { silent = true })
+vim.keymap.set("v", "L", "zL", { silent = true })
+
+-- yank path/filenames
+vim.keymap.set("n", "yf", "<cmd>let @+=expand('%:t')<CR>")
+vim.keymap.set("n", "yp", "<cmd>let @+=expand('%')<CR>")
+vim.keymap.set("n", "yP", "<cmd>let @+=expand('%:p')<CR>")
+vim.keymap.set("n", "yd", "<cmd>let @+=expand('%:h')<CR>")
+vim.keymap.set("n", "yD", "<cmd>let @+=expand('%:p:h')<CR>")
+
+-- read/write to shell
+vim.keymap.set("n", "<leader>xr", ":r!")
+vim.keymap.set("n", "<leader>xR", "!!$SHELL")
+vim.keymap.set("n", "<leader>xs", "<cmd>.w !$SHELL<CR>")
+vim.keymap.set("x", "<leader>xs", "<cmd>.w !$SHELL<CR>")
+vim.keymap.set("n", "<leader>xS", "<cmd>w !$SHELL<CR>")
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+local opt = vim.opt
+
+opt.hlsearch = false
+opt.guicursor = nil
+opt.conceallevel = nil
+opt.scrolloff = 8
+opt.listchars:append({ lead = "." })
+opt.listchars:append({ trail = "·" })
+opt.listchars:append({ tab = "▸ " })
+opt.listchars:append({ extends = "»" })
+opt.listchars:append({ precedes = "«" })
+opt.listchars:append({ nbsp = "⣿" })

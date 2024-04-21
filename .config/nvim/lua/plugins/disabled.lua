549196ffa1a81438fcf7358f315b13ebd2d2ed19
zaerald <zaerald.zd@gmail.com>
2024-04-21 13:28:22 +0800

    feat(nvim): update lazyvim

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
 }

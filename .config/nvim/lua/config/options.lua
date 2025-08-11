83660c91bd05c0de8fb4e402f6d2522b83b16fc2
zaerald <zaerald.zd@gmail.com>
2025-08-11 17:14:35 +0800

    feat(nvim): update search config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,6 +5,11 @@ vim.o.wrap = false
 vim.g.snacks_animate = false
 
 opt.hlsearch = false
+opt.incsearch = true
+opt.smartcase = true
+opt.showmatch = true
+opt.ignorecase = true
+opt.wrapscan = false
 opt.guicursor = "n-v-c-sm-i-ci-ve:block,r-cr-o:hor20"
 opt.conceallevel = 2
 opt.scrolloff = 8
